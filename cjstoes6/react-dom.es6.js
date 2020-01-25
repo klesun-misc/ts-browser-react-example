82dfa07cefa8012f63dbc11461e3817af398621f
Artur Klesun <arturklesun@gmail.com>
2020-01-25 11:34:48 +0200

    Move side-effects imports to the es6 wrappers they are used in

--- cjstoes6/react-dom.es6.js
+++ cjstoes6/react-dom.es6.js
@@ -1,1 +1,4 @@
+import 'https://unpkg.com/react@16/umd/react.production.min.js';
+import 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js';
+
 export default window.ReactDOM;
--- cjstoes6/react.es6.js
+++ cjstoes6/react.es6.js
@@ -1,1 +1,3 @@
+import 'https://unpkg.com/react@16/umd/react.production.min.js';
+
 export default window.React;
